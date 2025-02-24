5ac79e7cca06898c53351aa6fa2639753669ef85
ingridweber <ingrid.weber@hoedlmayr.com>
2025-02-24 14:46:29 +0100

    username error fixed

--- 02_Applications/Teamviewer/intunewin/Install.bat
+++ 02_Applications/Teamviewer/intunewin/Install.bat
@@ -1,6 +1,6 @@
 start /wait msiexec.exe /i "%~dp0\TeamViewer_Host.msi" /qn CUSTOMCONFIGID=vtcuuvt
 ping -n 30 127.0.0.1>nul
-"C:\Program Files\TeamViewer\TeamViewer.exe" assign --api-token=5735986-zZjFqFbaFGpsa7MIBkJT --reassign --alias "%COMPUTERNAME% | %USERNAME" --grant-easy-access
+"C:\Program Files\TeamViewer\TeamViewer.exe" assign --api-token=5735986-zZjFqFbaFGpsa7MIBkJT --reassign --alias "%COMPUTERNAME% | %USERNAME%" --grant-easy-access
 ping -n 30 127.0.0.1>nul
 net stop teamviewer
 ping -n 10 127.0.0.1>nul
